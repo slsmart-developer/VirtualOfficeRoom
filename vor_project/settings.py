793102e71fcc9d7c159d8c7fcc9bdacc4ced6a30
sujith jeewantha <sujith.j@zillionica.com>
2022-09-17 22:15:15 +0530

    static file changes

--- vor_project/settings.py
+++ vor_project/settings.py
@@ -180,7 +180,7 @@ EMAIL_HOST_PASSWORD = 'wjuumqlnsjlltepy'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
